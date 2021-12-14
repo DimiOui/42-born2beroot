5312c6657ab8ec0c8f046e0d4b1bb49c126b4016
dimi <dimitri.paccagnini@laposte.net>
2021-12-14 16:21:01 +0100

    config files to use

---
+++ monitoring.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "#Architecture : $(uname -a)"
+echo "#CPU Physical : "
+echo "#vCPU : "
+echo "#Memory Usage : "
+echo "#Disk Usage : "
+echo "#CPU Load : "
+echo "#Last boot : "
+echo "#LVM Use : "
+echo "#TCP Connections : "
+echo "#User Log : "
+echo "#Network : "
+echo "#Sudo : "
